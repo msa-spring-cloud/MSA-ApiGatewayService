54a47f3173b6db136037e6069634c08044e80eca
= <roman14@naver.com>
2022-12-08 23:27:01 +0900

    Initial commit

---
+++ src/main/java/com/roman14/apigatewayservice/MsaApiGatewayServiceApplication.java
@@ -0,0 +1,15 @@
+package com.roman14.apigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaApiGatewayServiceApplication
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(MsaApiGatewayServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/roman14/apigatewayservice/MsaApiGatewayServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.roman14.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaApiGatewayServiceApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
